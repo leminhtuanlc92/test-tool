ddf0e34ec4de012e0d979f88d709e97217bafd0f
leminhtuan <leminhtuanlc92@gmail.com>
2021-06-30 17:04:32 +0700

    no message

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import BigNumber from 'bignumber.js';
+import TronWeb from "tronweb";
+
+import './App.css';
+
+const contract = 'TQhAbjfAY9GDTi3oDX1UaKPFwJwSYfuREN'
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [privateKey, setPrivateKey] = useState('')
+  const [amount, setAmount] = useState('')
+  const [loop, setLoop] = useState(1)
+  const handleInvest = async (amount: number, privateKey: string, loop: number) => {
+    const tronWeb = new TronWeb({
+      fullNode: 'https://api.nileex.io/',
+      solidityNode: 'https://api.nileex.io/',
+      eventServer: 'https://api.nileex.io/',
+      privateKey: privateKey
+    });
+    console.log('LL', amount, privateKey, loop, contract)
+    for (let i = 0; i < loop; i++) {
+      try {
+        const memo = `provide`
+        const unSignedTxn = await tronWeb.transactionBuilder.sendTrx(
+          contract,
+          +new BigNumber(amount).multipliedBy(1e6)
+        );
+        console.log('Invest unSignedTxn', unSignedTxn)
+        const unSignedTxnWithNote = await tronWeb.transactionBuilder.addUpdateData(unSignedTxn, memo, 'utf8');
+        // console.log('Invest unSignedTxnWithNote', unSignedTxnWithNote)
+        const signedTxn = await tronWeb.trx.sign(unSignedTxnWithNote, privateKey);
+        // console.log('Invest signedTxn', signedTxn)
+        const result = await tronWeb.trx.sendRawTransaction(signedTxn);
+        console.log('result', result)
+        if (result) {
+          console.log(`Hiến máu lần ${i} thành công!`)
+        }
+      } catch (error) {
+        setLoading(false)
+        console.log('submit Invest power error', error)
+      }
+      finally {
+        setLoading(false)
+      }
+    }
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="privateKey">Private key của cưng là gì?</label>
+        <input value={privateKey} onChange={(e) => setPrivateKey(e.target.value)} id="privateKey" />
+
+        <label htmlFor="amount">Hiến bao lít máu?</label>
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" />
+
+        <label htmlFor="looping">Hiến bao lần nà?</label>
+        <input value={loop} onChange={(e) => setLoop(+e.target.value)} id="looping" type="number" />
+
+        <button onClick={() => !loading && handleInvest(+amount, privateKey, loop)}>Hiến!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
