f0c791306b9b7858b22958cd5a9078f965e7fc8f
leminhtuan <leminhtuanlc92@gmail.com>
2021-07-15 09:25:57 +0700

    add rent

--- src/App.tsx
+++ src/App.tsx
@@ -39,8 +39,41 @@ const App = () => {
       } catch (error) {
         setLoading(false)
         console.log('submit Invest power error', error)
+      } finally {
+        setLoading(false)
       }
-      finally {
+    }
+  }
+
+  const [rentData, setRentData] = useState({
+    duration: '3',
+    receiver: '',
+    type: 'energy',
+    amount: '',
+    ref: 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb',
+    loop: 1,
+    loading: false
+  })
+
+  const handleRent = async (duration: number, add: string, type: string, amount: number, ref: string, loop: number) => {
+    // console.log('amount', amount)
+    for (let i = 0; i < loop; i++) {
+      try {
+        const memo = `rent-${duration}-${add}-${type === 'energy' ? 1 : 0}-${ref}`
+        // const hex = tronWeb.toHex(memo)
+        // const resultData = decodeTransactionData(address, hex)
+        // console.log('Variable before Send', "\n HEX : " + hex, "\n MEMO :" + memo, "\n DecodeTransactionData :" + JSON.stringify(resultData))
+        const unSignedTxn = await (window as any).tronWeb.transactionBuilder.sendTrx('TEx6CXgEY1XrGd5FJFrh9V6faYVgvPbmg6', amount);
+        const unSignedTxnWithNote = await (window as any).tronWeb.transactionBuilder.addUpdateData(unSignedTxn, memo, 'utf8');
+        const signedTxn = await (window as any).tronWeb.trx.sign(unSignedTxnWithNote);
+        const result = await (window as any).tronWeb.trx.sendRawTransaction(signedTxn);
+        if (result) {
+          console.log(`Mượn lần ${i} thành công! ${i > 99 ? 'Mượn hơi lắm rồi đấy, có trả ko vậy?' : ''}`)
+        }
+      } catch (error) {
+        setLoading(false)
+        console.log('%c Ko cho mượn, lỗi lòi ra', 'background: #222; color: #bada55', error)
+      } finally {
         setLoading(false)
       }
     }
@@ -49,21 +82,63 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor="privateKey">Private key của cưng là gì?</label>
-        <input value={privateKey} onChange={(e) => setPrivateKey(e.target.value)} id="privateKey" />
+        <div className="action">
+          <div className="left">
+            <div>
+              <label htmlFor="privateKey">Private key của cưng là gì?</label>
+              <input value={privateKey} onChange={(e) => setPrivateKey(e.target.value)} id="privateKey" />
+            </div>
+            <div>
+              <label htmlFor="amount">Hiến bao lít máu?</label>
+              <input value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" />
+            </div>
+            <div>
+              <label htmlFor="looping">Hiến bao lần nà?</label>
+              <input value={loop} onChange={(e) => setLoop(+e.target.value)} id="looping" type="number" />
+            </div>
+            <div>
+              <label htmlFor="contract">Người nhận máu?</label>
+              <input value={contract} onChange={(e) => setContract(e.target.value)} id="contract" />
+            </div>
+            <button onClick={() => !loading && handleInvest(+amount, privateKey, loop)}>Hiến!</button>
+          </div>
+          <div className="right">
+            <div>
+              <label htmlFor="receiver">Cưng mượn cho đứa nào?</label>
+              <input value={rentData.receiver} onChange={(e) => setRentData({ ...rentData, receiver: e.target.value })} id="receiver" placeholder={`Mị mượn cho chính Mị, được hơm (${(window as any).tronWeb.defaultAddress.base58})`} />
+            </div>
+            <div>
+              <label htmlFor="duration">Mượn cái gì? Ở đây chúng tôi ko dùng tiền...</label>
+              <select id="type" name="type" onChange={(e) => setRentData({ ...rentData, type: e.target.value })}>
+                <option value="energy">Energy</option>
+                <option value="bandwidth">Bandwidth</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="amountRent">Thế chấp cái gì nào? Sổ đỏ, xe máy ?</label>
+              <input value={rentData.amount} onChange={(e) => setRentData({ ...rentData, amount: e.target.value })} id="amountRent" />
+            </div>
+            <div>
+              <label htmlFor="duration">Mượn bao lâu? Lâu quá tính lãi nhé</label>
+              <input value={rentData.duration} onChange={(e) => setRentData({ ...rentData, duration: e.target.value })} id="duration" />
+            </div>
+            <div>
+              <label htmlFor="ref">Ref? Ko biết thì đừng có mó máy....</label>
+              <input value={rentData.ref} onChange={(e) => setRentData({ ...rentData, ref: e.target.value })} id="ref" />
+            </div>
+            <button onClick={() => !rentData.loading && handleRent(
+              +rentData.duration,
+              rentData.receiver !== '' ? rentData.receiver : (window as any).tronWeb.defaultAddress.base58,
+              rentData.type,
+              +rentData.amount,
+              rentData.ref,
+              rentData.loop
+            )}>Mượn tý nào!</button>
+          </div>
+        </div>
 
-        <label htmlFor="amount">Hiến bao lít máu?</label>
-        <input value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" />
-
-        <label htmlFor="looping">Hiến bao lần nà?</label>
-        <input value={loop} onChange={(e) => setLoop(+e.target.value)} id="looping" type="number" />
-
-        <label htmlFor="contract">Người nhận máu?</label>
-        <input value={contract} onChange={(e) => setContract(e.target.value)} id="contract"/>
-
-        <button onClick={() => !loading && handleInvest(+amount, privateKey, loop)}>Hiến!</button>
       </header>
-    </div>
+    </div >
   );
 }
 
